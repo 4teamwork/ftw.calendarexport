50b8727593ca6d373b0b30c0a9264f63dc206480
Julian Infanger <j.infanger@4teamwork.ch>
2011-06-24 09:01:11 +0000

    Issue #331 Zusaetzliche Funktionen Terminkalender: end date has to be until 23:59:59

--- ftw/calendarexport/results.py
+++ ftw/calendarexport/results.py
@@ -24,8 +24,10 @@ class CalendarExportResults(BrowserView):
         """
         catalog = getToolByName(self.context, 'portal_catalog')
         if self.request.form.get('from','') and self.request.form.get('to',''):
-            start = DateTime(self.request.form.get('from','')).Date()
-            end = '%s 23:59:59' % DateTime(self.request.form.get('to','')).Date()
+            start = self.request.form.get('from','').split('.')
+            start = DateTime(*[int(p) for p in start])
+            end = self.request.form.get('to','').split('.')
+            end = DateTime(*[int(p) for p in end+[23,59,59]])
             return catalog(dict(
                 portal_type='Event',
                 start = {'range':'min', 'query': start},
