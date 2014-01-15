47400cd06246860497ec5cffed1bc79b8b073949
Timon Tschanz <timon.tschanz@4teamwork.ch>
2014-01-15 16:22:12 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
-version = open('ftw/calendarexport/version.txt').read().strip()
+version = '1.1.dev0'
 maintainer = 'Julian Infanger'
+
+tests_require = ['plone.app.testing',
+                 ]
 
 setup(name='ftw.calendarexport',
       version=version,
       description="Export function for ftw.calendar (Maintainer: %s)" % maintainer,
-      long_description=open("README.txt").read() + "\n" + \
+      long_description=open("README.rst").read() + "\n" + \
           open("docs/HISTORY.txt").read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -17,15 +20,17 @@ setup(name='ftw.calendarexport',
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
-      author='%s, 4teamwork GmbH' % maintainer,
+      author='4teamwork AG',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
-      url='http://psc.4teamwork.ch/',
+      url='http://github.com/4teamwork/ftw.calendarexport',
       license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
       install_requires=[
         'setuptools',
         'ftw.calendar',
